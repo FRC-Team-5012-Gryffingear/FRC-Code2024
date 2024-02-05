af3632eebe6e9e49c401a06f68d771becf3e06f2
Trollguyjr <90011577+Trollguyjr@users.noreply.github.com>
2024-02-04 23:58:30 -0800

    Json file

--- Test/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ Test/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -4,11 +4,14 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ExampleSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
+  
   public ExampleSubsystem() {}
 
   /**
