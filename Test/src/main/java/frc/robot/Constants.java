a870280cc8045c6d5cd3138c40430a088297d416
Trollguyjr <90011577+Trollguyjr@users.noreply.github.com>
2025-01-03 17:25:47 -0800

    Added Swerve auto Movement(not tested)

--- Test/src/main/java/frc/robot/Constants.java
+++ Test/src/main/java/frc/robot/Constants.java
@@ -6,6 +6,7 @@ package frc.robot;
 
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.units.Unit;
 
@@ -78,6 +79,12 @@ public final class Constants {
     public static final double KP = 0.4;
     public static final double KI = 0;
     public static final double KD = 0;
+
+    public static final TrapezoidProfile.Constraints thetaConstraints = 
+            new TrapezoidProfile.Constraints(
+                    Constants.Max_Angular,
+                    10
+            );
   }
 
   public static class OperatorConstants {
--- Test/src/main/java/frc/robot/RobotContainer.java
+++ Test/src/main/java/frc/robot/RobotContainer.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import frc.robot.Constants.ModConstants;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.AutoSimple;
 import frc.robot.commands.Autos;
@@ -20,13 +21,27 @@ import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.HookSubsystem;
 import frc.robot.subsystems.IntakeSubsys;
 import frc.robot.subsystems.LedSubsystem;
+import frc.robot.subsystems.SwerveMod;
 import frc.robot.subsystems.SwerveSubsystem;
 import frc.robot.subsystems.VisionSub;
 import frc.robot.subsystems.limey;
+
+import java.util.List;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -108,6 +123,44 @@ public class RobotContainer {
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
     //return Autos.exampleAuto(null);
-    return AutoS;
+    //CREATING A TRAJECTORY RAHHHHH
+    // 10 m/s^2 represents accel at reg grav basically
+    TrajectoryConfig trajectoryConfig = new TrajectoryConfig(
+        Constants.Max_velo, 
+        10)
+              .setKinematics(Constants.kinematics);
+    
+    Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
+            new Pose2d(0,0, new Rotation2d(0)),
+            List.of(
+                      new Translation2d(1,0),
+                      new Translation2d(1,-1)
+            ),
+            new Pose2d(2,-1,Rotation2d.fromDegrees(180)),
+            trajectoryConfig
+    );
+
+    PIDController xController = new PIDController(ModConstants.KP, 0, 0);
+    PIDController yController = new PIDController(ModConstants.KP, 0, 0);
+    ProfiledPIDController thetacontroller  = new ProfiledPIDController(ModConstants.KP, 0, 0, ModConstants.thetaConstraints);
+
+    thetacontroller.enableContinuousInput(-Math.PI, Math.PI);
+
+    //Constructs a command to follow traj
+    SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(
+      trajectory,
+      swerveSubsys::getPose, // Use instance method
+      Constants.kinematics,
+      xController,
+      yController,
+      thetacontroller,
+      swerveSubsys::setModStates, // Use instance method
+      swerveSubsys);
+
+    return new SequentialCommandGroup(
+      new InstantCommand(() -> swerveSubsys.resetPose(trajectory.getInitialPose())),
+      swerveControllerCommand,
+      new InstantCommand(() -> swerveSubsys.stopMods())
+    );
   }
 }
